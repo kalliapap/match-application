e96282f0aee5282d1d0c97153717d34f911e3a0b
kalliapap <kalliapap7@gmail.com>
2024-11-06 20:43:00 +0200

    Added match odd exceptions

---
+++ match-server/src/main/java/com/match/dev/exception/MatchOddNotFoundException.java
@@ -0,0 +1,9 @@
+package com.match.dev.exception;
+
+public class MatchOddNotFoundException extends BaseException{
+
+    public MatchOddNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ match-server/src/main/java/com/match/dev/exception/MatchOddValidationFailedException.java
@@ -0,0 +1,9 @@
+package com.match.dev.exception;
+
+public class MatchOddValidationFailedException extends BaseException{
+
+    public MatchOddValidationFailedException(String message) {
+        super(message);
+    }
+
+}
