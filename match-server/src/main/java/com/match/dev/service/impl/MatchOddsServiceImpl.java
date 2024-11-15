5f8e7cef0c5a64356a7392bc9f07e7183b7744ad
kalliapap <kalliapap7@gmail.com>
2024-11-15 17:19:25 +0200

    Removed unused injection

--- match-server/src/main/java/com/match/dev/service/impl/MatchOddsServiceImpl.java
+++ match-server/src/main/java/com/match/dev/service/impl/MatchOddsServiceImpl.java
@@ -15,7 +15,6 @@ import com.match.dev.util.MatchUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
